9d0f176364f728d4f3ec10764f382a077e3fb67f
zonda1 <amt.ilyin@gmail.com>
2021-09-27 22:18:38 +0300

    Добавлена адаптация для трех экранов

--- source/js/menu.js
+++ source/js/menu.js
@@ -7,14 +7,6 @@ iconBurger.classList.remove('no-js');
 navMain.classList.remove('no-js');
 
 iconBurger.addEventListener('click', function () {
-  // if (navMain.classList.contains('closed')) {
-  //   navMain.classList.remove('closed');
-  //   navMain.classList.add('active');
-  // } else {
-  //   navMain.classList.add('closed');
-  //   navMain.classList.remove('active');
-  // }
   iconBurger.classList.toggle('active');
   navMain.classList.toggle('active');
-
 });
